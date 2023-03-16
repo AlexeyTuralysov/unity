3c44e0016efcd840c5e1817259a2f9536eee0c74
AlexeyMarkul <72796090+AlexeyMarkul@users.noreply.github.com>
2023-03-16 19:51:34 +0900

    first commit

---
+++ New Unity Project/Assets/script/MouseLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+     public enum RotationAxes
+  
+    {
+        MouseXAndY=0,
+        MouseX=1,
+        MouseY=2   
+    }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    void Update()
+    {
+        if axes = (RotationAxes.MouseX)
+    }
+    
+    
+     else if (axes = RotationAxes.MouseY)
+    
+   
+}
